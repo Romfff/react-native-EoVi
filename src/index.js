fd495b0ed4085c9404fdbb9e5c2f181f65f3792c
Romfff <vy.l.2005@gmail.com>
2025-08-06 19:17:10 +0700

    fix api key

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import stRoutes from "./routes/stRoutes.js";
 import { connectDB } from "./lib/db.js";
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 // ✅ tăng giới hạn request body lên 10MB (hoặc 20MB nếu cần)
 app.use(express.json({ limit: "10mb" }));
